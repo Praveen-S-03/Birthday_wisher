de4e9988dfd22ef52f575895cdb158d734d6e545
Praveen-S-03 <connectwithpraveensuresh@gmail.com>
2025-07-26 21:47:32 +0530

    Delete GITHUB code directory

--- GITHUB code/IIS_finder/main.py
+++
@@ -1,9 +0,0 @@
-import requests
-api_endpoint = "http://api.open-notify.org/iss-now.json"
-#The above link is used to find the position of IIS
-response = requests.get(url=api_endpoint)
-response.raise_for_status()
-data = response.json()
-latitude = data["iss_position"]["latitude"]
-longitude = data["iss_position"]["longitude"]
-print(f"The International Space station is at {latitude} Latitude and {longitude} Longitude")
